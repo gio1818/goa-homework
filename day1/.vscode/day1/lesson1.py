67e436f6d3b4df5c5d882c4e5f17700d4f7e2e1f
devrisashviligiorgi76@gmail.com <devrisashviligiorgi76@gmail.com>
2025-02-02 23:57:10 +0400

    first commit

---
+++ day1/.vscode/day1/lesson1.py
@@ -0,0 +1,6 @@
+name = "giogi"
+surname = "devrisashvili"
+age = 15
+height = 186.1
+knows_programing="false"
+print( "my full name is " +name + " " +(surname)+ " i am "  +str(age)+" years old my height is "+str(height)+ " i am very good programist " +str(knows_programing))
