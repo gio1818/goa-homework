fa80a8d738691e0fb32097d842421ca97ea302ee
devrisashviligiorgi76@gmail.com <devrisashviligiorgi76@gmail.com>
2025-03-10 18:32:32 +0400

    second commit

---
+++ day04/classwork/classwork.py
@@ -0,0 +1,20 @@
+# name="giorgi"
+# lastname="devrisashvili"
+# fullname=("giorgi" +" "+ "devrisashvili")
+# print(fullname)
+
+# სჭირდება ბრჭყალები
+car=lambo 
+
+
+#პირველი უნდა იყოს month cvladi
+print(month)
+month=(march) 
+
+#სჭირდება 2 ბრჭყალი
+sun=cold"     
+
+# სჭირდება = -->ამის მაგივრად
+continent-->"america"# ბრჭყალები არ სჭირდება int
+num1="223"
+# debugging არის შეცდომის გასწორება
