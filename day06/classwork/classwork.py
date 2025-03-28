2466efc876fafe1aebd8ecc93375efefbcf62276
devrisashviligiorgi76@gmail.com <devrisashviligiorgi76@gmail.com>
2025-03-28 15:10:44 +0400

    second commit

---
+++ day06/classwork/classwork.py
@@ -0,0 +1,7 @@
+num = int(input("num: "))
+num1 = int(input("num1: "))
+print(num + num1)
+print(num - num1)
+print(num * num1)
+print(num / num1)
+# snake case არის ქვედა ტირე მას ვიყენებთ space მაგივრად case sensitivity არის ციფრების განსხვავება მაგალითად C c
---
+++ day06/homework/homework.py
@@ -0,0 +1,18 @@
+sport=input("enter your sport:")
+print(sport)
+
+name = input("name: ")
+surname = input("surname: ")
+age = input("age: ")
+city = input("hometown : ")
+hobby = input("hobby: ")
+print(name +" "+ surname +" "+str(age)+"hometown" + " "+hobby)
+
+num = int(input("num1: "))
+result=(num-20)*3
+print(result) 
+num1=int(input("num1: "))
+num2=int(input("num2: "))
+num3=int(input("num3:"))
+result=(num1 +num2 +num3) /3
+print(result) 
