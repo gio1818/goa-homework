92d7add250173f4313204d056ea7f56f09ed6398
devrisashviligiorgi76@gmail.com <devrisashviligiorgi76@gmail.com>
2025-04-07 00:15:27 +0400

    second commit

---
+++ day08/homework/homework1.py
@@ -0,0 +1,8 @@
+name =(input("შეიტანეთ თქვენი სახელი: "))
+print(type(name))
+
+age=input("age:")
+age=str(age)
+print(type(age))
+
+# str() ფუნქცია აქცევს ყველაფერს სტრინგად int() აქცევს ყველაფერს ინტად float() აქცევს ყველაფერს ფლოათად
