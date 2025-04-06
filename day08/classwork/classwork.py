5b9625de588c5b544ad8909ca9d8ab69330a20fa
devrisashviligiorgi76@gmail.com <devrisashviligiorgi76@gmail.com>
2025-04-06 23:46:55 +0400

    second commit

---
+++ day08/classwork/classwork.py
@@ -0,0 +1,12 @@
+num1= input("enter number1:")
+num2 =input("enter number2:")
+print(int(num1) + int(num2))
+print(int(num1) - int(num2))
+print(int(num1) / int(num2))
+print(int(num1) * int(num2))
+
+# კონკატინაცია არის ორი სტრინგის შეერთება
+name =input("your name:")
+surname=input("your surname:")
+age=("your age:")
+print(name + " " + surname +" "+ age )
